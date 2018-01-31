389fa51ffbd6739d6c0a80803017c13dc41b4c89
shijun <shijun1985911@163.com>
2018-01-31 19:50:14 +0800

    push

--- src/test.js
+++ src/test.js
@@ -2,11 +2,11 @@ if (1) {
   let a = 10;
   console.log(a);
 }
+
 else {
   let a = 20 ;
   console.log(a);
 }
-
 
 function aaa (){
   const a = [];
@@ -15,18 +15,12 @@ function aaa (){
   console.log(a);
 }
 
-aaa()
-
-
 ()=>{return 1};
-
 
 let aaa = (app) => {
   app.pop;
   return app
 }
-
-
 
 let xx = (x) => {
   return x*x;
